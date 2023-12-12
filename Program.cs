32135bf8de4170f43d7e1b80a0324f2412a79e8c
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2023-12-12 19:38:52 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿/* DT071G - Programmering i C#.NET
+ * Project
+ * Linn Eriksson, HT23
+ */
+
+namespace project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
