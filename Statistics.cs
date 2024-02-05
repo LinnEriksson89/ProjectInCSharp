40b45b52ded547f7c3515fabf50aea8c200485c1
LinnEriksson89 <123004529+LinnEriksson89@users.noreply.github.com>
2024-02-05 17:56:47 +0100

    Adding statistics to game

---
+++ Statistics.cs
@@ -0,0 +1,27 @@
+/* DT071G - Programmering i C#.NET
+ * Project
+ * Linn Eriksson, HT23
+ */
+
+using System.Text.Json;
+
+
+namespace project
+{
+    class Statistics
+    {
+        public Statistics (int turns, string word, bool winner)
+        {
+            //Constructor of statistics.
+            Word = word;
+            Turns = turns;
+            Winner = winner;
+            Time = DateTime.Now;
+        }
+
+        public string Word {get; set;}    
+        public int Turns {get; set;}
+        public bool Winner {get; set;}
+        public DateTime Time {get; set;}
+    }
+}
